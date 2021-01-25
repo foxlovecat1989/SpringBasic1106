ae27f5b70735ca1ccbb3468dcaa37b8f06b69ec0
foxlo <foxlo@DESKTOP-4R8BSQ7>
2021-01-25 21:24:06 +0800

    DI

---
+++ src/test/java/di/Test_Student2.java
@@ -0,0 +1,19 @@
+package di;
+
+import com.mycompany.springbasic1106.di.Student;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test_Student2 {
+    @Test
+    public void test() {
+        ClassPathXmlApplicationContext ctx = 
+                new ClassPathXmlApplicationContext("beans-config.xml");
+        Student s1 = (Student)ctx.getBean("s1");
+        // System.out.println(s1);
+        Student s2 = ctx.getBean("s2", Student.class);
+        // System.out.println(s2);
+        Student s3 = ctx.getBean("s3", Student.class);
+        System.out.println(s3);
+    }
+}
